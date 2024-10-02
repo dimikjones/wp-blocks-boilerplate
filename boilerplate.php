181753d36e4d5e6d0b778ed3118b2353daeb10af
Marko Dimitrijevic <mdimitrijevic84@gmail.com>
2024-10-02 14:46:45 +0200

    initiall

--- boilerplate.php
+++ boilerplate.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name:       Boilerplate
  * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
- * Requires PHP:      7.0
+ * Requires at least: 6.6
+ * Requires PHP:      7.2
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
